d307ad3b6f6984f632e7c89e22993bc4f49ef042
brtiago <727320+brtiago@users.noreply.github.com>
2023-10-18 18:33:39 -0300

    feat: create method listar

--- src/main/java/com/amedigital/challenge/controller/PlanetsController.java
+++ src/main/java/com/amedigital/challenge/controller/PlanetsController.java
@@ -1,11 +1,14 @@
 package com.amedigital.challenge.controller;
 
+import com.amedigital.challenge.dto.PlanetListDTO;
 import com.amedigital.challenge.dto.PlanetsDTO;
 import com.amedigital.challenge.dto.PlanetDetailsDTO;
 import com.amedigital.challenge.repository.PlanetsRepository;
 import com.amedigital.challenge.service.PlanetsService;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -23,6 +26,12 @@ public class PlanetsController {
     @PostMapping
     public void cadastrar(@RequestBody PlanetsDTO dto) {
         service.cadastrar(dto);
+    }
+
+    @GetMapping
+    public ResponseEntity <Page<PlanetListDTO>> listar(Pageable paginacao){
+        var page = repository.findAll(paginacao).map(PlanetListDTO::new);
+        return ResponseEntity.ok(page);
     }
 
     @Transactional
