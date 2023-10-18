8180ac0dcdd7fae1b1051a7d4b380e5098d19414
brtiago <727320+brtiago@users.noreply.github.com>
2023-10-18 18:06:33 -0300

    feat: create methods remover and detalhar

--- src/main/java/com/amedigital/challenge/controller/PlanetsController.java
+++ src/main/java/com/amedigital/challenge/controller/PlanetsController.java
@@ -1,8 +1,12 @@
 package com.amedigital.challenge.controller;
 
 import com.amedigital.challenge.dto.PlanetsDTO;
+import com.amedigital.challenge.dto.PlanetDetailsDTO;
+import com.amedigital.challenge.repository.PlanetsRepository;
 import com.amedigital.challenge.service.PlanetsService;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -12,9 +16,27 @@ public class PlanetsController {
     @Autowired
     private PlanetsService service;
 
+    @Autowired
+    private PlanetsRepository repository;
+
+    @Transactional
     @PostMapping
     public void cadastrar(@RequestBody PlanetsDTO dto) {
         service.cadastrar(dto);
+    }
+
+    @Transactional
+    @DeleteMapping("/{id}")
+    public ResponseEntity remover(@PathVariable Long id){
+        var planet = repository.getReferenceById(id);
+        planet.excluir();
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity detalhar(@PathVariable Long id){
+        var planet = repository.getReferenceById(id);
+        return ResponseEntity.ok(new PlanetDetailsDTO(planet));
     }
 
 }
