87171933f440f540b727c1629c91c787ef63fab3
brtiago <727320+brtiago@users.noreply.github.com>
2023-10-23 18:52:41 -0300

    feat: create method findByName

--- src/main/java/com/amedigital/challenge/repository/PlanetsRepository.java
+++ src/main/java/com/amedigital/challenge/repository/PlanetsRepository.java
@@ -3,5 +3,8 @@ package com.amedigital.challenge.repository;
 import com.amedigital.challenge.domain.Planets;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PlanetsRepository extends JpaRepository<Planets, Long> {
+    Optional<Planets> findByName(String name);
 }
