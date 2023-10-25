b861d35de2687728bfdaa16f9ab7782840827f4a
brtiago <727320+brtiago@users.noreply.github.com>
2023-10-25 20:28:03 -0300

    feat: implementação do método cadastrar

--- src/main/java/com/amedigital/challenge/controller/PlanetsController.java
+++ src/main/java/com/amedigital/challenge/controller/PlanetsController.java
@@ -1,16 +1,17 @@
 package com.amedigital.challenge.controller;
 
-import com.amedigital.challenge.dto.PlanetListDTO;
+import com.amedigital.challenge.domain.Planets;
 import com.amedigital.challenge.dto.PlanetsDTO;
 import com.amedigital.challenge.dto.PlanetDetailsDTO;
 import com.amedigital.challenge.repository.PlanetsRepository;
 import com.amedigital.challenge.service.PlanetsService;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/planets")
@@ -24,34 +25,34 @@ public class PlanetsController {
 
     @Transactional
     @PostMapping
-    public void cadastrar (@RequestBody PlanetsDTO dto) {
-        service.cadastrar(dto);
+    public ResponseEntity<Planets> cadastrar (@RequestBody PlanetsDTO dto) {
+        Planets newPlanet = service.cadastrar(dto);
+        return new ResponseEntity<>(newPlanet, HttpStatus.CREATED);
     }
 
     @GetMapping
-    public ResponseEntity <Page<PlanetListDTO>> listar(Pageable paginacao){
-        var page = repository.findAll(paginacao).map(PlanetListDTO::new);
-        return ResponseEntity.ok(page);
+    public ResponseEntity <List<Planets>> listar(){
+        var planets = service.listar();
+        return new ResponseEntity<>(planets, HttpStatus.OK);
     }
 
     @Transactional
     @DeleteMapping("/{id}")
-    public ResponseEntity remover(@PathVariable Long id){
-        var planet = repository.getReferenceById(id);
-        planet.excluir();
+    public ResponseEntity remover(@PathVariable Long id) throws Exception {
+        service.removerById(id);
         return ResponseEntity.noContent().build();
     }
 
 
     @GetMapping("/{id}")
-    public ResponseEntity buscarPorId(@PathVariable Long id){
-        var planet = repository.getReferenceById(id);
+    public ResponseEntity buscarPorId(@PathVariable Long id) throws Exception {
+        var planet = service.findById(id);
         return ResponseEntity.ok(new PlanetDetailsDTO(planet));
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity buscarPorNome(@PathVariable Long id){
-        var planet = repository.getReferenceById(id);
+    @GetMapping("/{name}")
+    public ResponseEntity findByName(@PathVariable String name) throws Exception {
+        var planet = service.findByName(name);
         return ResponseEntity.ok(new PlanetDetailsDTO(planet));
     }
 
