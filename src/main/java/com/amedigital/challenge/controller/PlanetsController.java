610bc321b01ae1e602ff317046c5f8612021197b
brtiago <727320+brtiago@users.noreply.github.com>
2023-10-25 21:33:40 -0300

    feat: implementação das annotations Operation

--- src/main/java/com/amedigital/challenge/controller/PlanetsController.java
+++ src/main/java/com/amedigital/challenge/controller/PlanetsController.java
@@ -5,6 +5,7 @@ import com.amedigital.challenge.dto.PlanetsDTO;
 import com.amedigital.challenge.dto.PlanetDetailsDTO;
 import com.amedigital.challenge.repository.PlanetsRepository;
 import com.amedigital.challenge.service.PlanetsService;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -24,12 +25,13 @@ public class PlanetsController {
     private PlanetsRepository repository;
 
     @Transactional
+    @Operation(summary = "Adicionar um planeta (com nome, clima e terreno)", method = "POST")
     @PostMapping
     public ResponseEntity<Planets> cadastrar (@RequestBody PlanetsDTO dto) {
         Planets newPlanet = service.cadastrar(dto);
         return new ResponseEntity<>(newPlanet, HttpStatus.CREATED);
     }
-
+    @Operation(summary = "Listar todos os planetas", method = "GET")
     @GetMapping
     public ResponseEntity <List<Planets>> listar(){
         var planets = service.listar();
@@ -37,19 +39,20 @@ public class PlanetsController {
     }
 
     @Transactional
+    @Operation(summary = "Remover planetas pelo id", method = "DELETE")
     @DeleteMapping("/{id}")
     public ResponseEntity remover(@PathVariable Long id) throws Exception {
         service.removerById(id);
         return ResponseEntity.noContent().build();
     }
 
-
+    @Operation(summary = "Buscar planetas pelo id", method = "GET")
     @GetMapping("/{id}")
     public ResponseEntity buscarPorId(@PathVariable Long id) throws Exception {
         var planet = service.findById(id);
         return ResponseEntity.ok(new PlanetDetailsDTO(planet));
     }
-
+    @Operation(summary = "Buscar planetas pelo nome", method = "GET")
     @GetMapping("/{name}")
     public ResponseEntity findByName(@PathVariable String name) throws Exception {
         var planet = service.findByName(name);
